ef8f750cde70dfa8b7cd597e2a74e9a5a1d874fe
Jie Huang <jie.huang@aalto.fi>
2023-11-30 19:44:41 +0200

    add log2cif.py

---
+++ Workflow/Step3/README.md
@@ -0,0 +1,9 @@
+# Scripts used in step 3
+
+1. `log2cif.py`
+
+Convert the last frame of  QE output file to a cif, and output the H2 density.
+ 
+Usage: `python log2cif.py QE_logfile`
+
+
---
+++ Workflow/Step3/log2cif.py
@@ -0,0 +1,33 @@
+from ase.io import read, write
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python log2cif.py QE_log_file")
+    sys.exit(1)
+
+
+qe_out = sys.argv[1]
+
+# Read the last frame from QE output log file
+frame = read(qe_out, index=-1, format='espresso-out')
+lattice = frame.get_cell().tolist()
+print(f'Lattice parameters:\n{lattice}')
+
+total_atoms = len(frame)
+print('Number of total atoms: ', total_atoms)
+
+# Get the total energy for the last frame
+last_frame_energy = frame.get_total_energy()
+print('Total Energy: {} eV'.format(last_frame_energy))
+print('Total Energy: {} Ry'.format(last_frame_energy/13.605703976))
+
+hydrogen_atoms = [atom for atom in frame if atom.symbol == 'H']
+pairs_of_hydrogen_atoms = len(hydrogen_atoms) / 2.
+print('H2 number: {}'.format(pairs_of_hydrogen_atoms))
+volume = frame.get_volume()
+print(f'Volume: {volume:.2f} cubic Angstrom')
+
+pairs_per_volume = pairs_of_hydrogen_atoms / volume
+print(f'Pairs of hydrogen atoms per volume: {pairs_per_volume:.5f}')
+
+write('{}.cif'.format(qe_out), frame)
