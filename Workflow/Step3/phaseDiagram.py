1f19b46c623d17cad824441dccfe8985b514c942
Jie Huang <jie.huang@aalto.fi>
2023-12-08 03:27:53 +0200

    add phaseDiagram.py phasediagram.pdf

--- Workflow/Step3/phaseDiagram.py
+++ Workflow/Step3/phaseDiagram.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 crystals = ['AlH$_3$', 'XAlH$_4$', 'X$_5$Al$_3$H$_{14}$', 'X$_3$AlH$_6$', 'XH']
-atomic_fraction_of_XH = [0, 33.33, 45.45, 60, 100]
+atomic_fraction_of_XH = np.array([0, 33.33, 45.45, 60, 100])
 # Fomation energy E [(eV)/atom]   # AlH3  (X)AlH4           (X)5Al3H14        (X)3AlH6       (X)H
 formation_energies =   np.array([[0,    0.00940625384,    0.009556500753,  -0.0232288214,   0],  # Li
                                  [0,   -0.06348366911,   -0.0620795669,    -0.05635193381,  0],  # Na
@@ -18,17 +18,26 @@ formation_energies =   np.array([[0,    0.00940625384,    0.009556500753,  -0.02
                                  [0,   -0.1596555026,    -0.05658973646,   -0.1011775038,   0]]) # Cs
 
 
+convex_hull_idx_list = [[0, 3, 4],
+                        [0, 1, 2, 3, 4], 
+                        [0, 1, 3, 4], 
+                        [0, 1, 3, 4], 
+                        [0, 1, 3, 4]]
 
-
+numCrystals = len(crystals)
+markers = ['s','v' , 'o', 'D', '^']
 plt.figure(figsize=(5.5, 8))
-
-
 for i, X in enumerate(['Li', 'Na', 'K', 'Rb', 'Cs']):
     plt.subplot(5, 1, i+1)
-    plt.scatter(atomic_fraction_of_XH, formation_energies[i])
+    plt.axhline(y=0, color='gray', linestyle=':', lw=0.5)
+    for f in atomic_fraction_of_XH:
+        plt.axvline(x=f, color='gray', linestyle='--', lw=0.3)
+    plt.plot(atomic_fraction_of_XH[convex_hull_idx_list[i]], formation_energies[i][convex_hull_idx_list[i]], 'black', lw=0.6)
+    for j in range(numCrystals):
+        plt.scatter(atomic_fraction_of_XH[j], formation_energies[i][j], marker=markers[j], color='black', label=crystals[j])
     plt.tick_params(direction='in', labelsize=12)
     plt.xlim(-15, 115)
-    plt.ylim(-0.19, 0.04)
+    plt.ylim(-0.19, 0.06)
     plt.xlabel('Atomic fraction of (X)H', fontsize=18)
     if i ==2:
         plt.ylabel('E$_\mathrm{form}$ [eV/atom]', fontsize=18)
@@ -36,26 +45,10 @@ for i, X in enumerate(['Li', 'Na', 'K', 'Rb', 'Cs']):
         plt.tick_params(labelbottom=False)
         plt.xlabel('')
     plt.annotate(f'({chr(97+i)})', xy=(0.02, 0.8), xycoords="axes fraction", fontsize=16)
+    plt.annotate('X={}'.format(X), xy=(0.02, 0.1), xycoords="axes fraction", fontsize=16)
+    if i == 0:
+        plt.legend(ncol=5, columnspacing=0.1, handletextpad=0, loc='upper center', bbox_to_anchor=(0.5, 1.30), fontsize=12, frameon=False)
 plt.subplots_adjust(hspace=0.02, wspace=0.01, left=0.15, bottom=0.1, right=0.99, top=0.95)
-# plt.savefig('phasediaRbH.png')
-# plt.savefig('phasediaRbH.pdf')
+plt.savefig('phasediagram.png')
+plt.savefig('phasediagram.pdf')
 plt.show()
-
-
-
-# for i, crystal in enumerate(crystals):
-#     plt.text(rbh_percent[i]+8, formation_energy[i]+0.005, crystal, fontsize=16, ha='right', va='bottom')
-# plt.text(0.03, 0.1, '(a)', fontsize=16, fontweight='bold', transform=plt.gca().transAxes)
-
-# plt.subplot(2, 1, 2)
-# plt.scatter(rbh_percent, H2_density, color='green', marker='s')
-# plt.xlabel('Percentage of RbH', fontsize=18)
-# plt.ylabel('H$_2$ density [Å$^{-3}$]', fontsize=18)
-# plt.tick_params(direction='in', labelsize=12)
-# plt.xlim(-5, 112)
-# plt.ylim(0.006, 0.038)
-# for i, crystal in enumerate(crystals):
-#     plt.text(rbh_percent[i]+8, H2_density[i]+0.001, crystal, fontsize=16, ha='right', va='bottom')
-# plt.text(0.03, 0.1, '(b)', fontsize=16, fontweight='bold', transform=plt.gca().transAxes)
-
-
